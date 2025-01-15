4a9ecbd4f072174bbd32323edffeab5e18989e59
echocslsk <CardioLeo@gmail.com>
2025-01-14 23:56:09 -0700

    moves files into folders for chapters- it was getting out of hand

---
+++ ch02/ch02-03.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Here are some truth tables!")
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
---
+++ ch03/ch03-07.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// x := "Hello, World"
+	x := 5
+	fmt.Println(x)
+	dogsName := "Max"
+	fmt.Println("My dog's name is", dogsName)
+}
