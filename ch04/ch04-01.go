4687ed58806bb3126f09d46e83a6872c9de65ef6
echocslsk <CardioLeo@gmail.com>
2025-01-15 11:44:16 -0700

    implements fizzbuzz algorithm while messing around with the code from first 5ish pages of chapter 4 of Introducing Go

---
+++ ch04/ch04-01.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	i := 1
+	for i <= 100 {
+		// fmt.Println(i)
+		if (i % 3 == 0) && (i % 5 == 0) {
+                        fmt.Println("FizzBuzz")
+                } else if i % 3 == 0 {
+			fmt.Println("Fizz")
+		} else if i % 5 == 0 {
+			fmt.Println("Buzz")
+		}
+		i = i + 1
+	}
+}
+
+// transformed this into my first implementation of FizzBuzz
+// based on some of the code from the first five pages of chap.4
