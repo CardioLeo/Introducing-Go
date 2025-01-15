786d733dafd5861d6f3b22ca67a6a62305438759
echocslsk <CardioLeo@gmail.com>
2025-01-15 14:57:16 -0700

    adds new code from page 39, the elements list printing Lithium

---
+++ ch05/ch05-08.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	elements := make(map[string]string)
+	elements["H"] = "Hydrogen"
+	elements["He"] = "Helium"
+	elements["Li"] = "Lithium"
+	elements["Be"] = "Berryllum"
+	elements["B"] = "Boron"
+	elements["C"] = "Carbon"
+	elements["N"] = "Nitrogen"
+	elements["O"] = "Oxygen"
+	elements["F"] = "Fluorine"
+	elements["Ne"] = "Neon"
+
+	fmt.Println(elements["Li"])
+	/*
+	for i := 0; i > len(elements[]); i++ {
+		fmt.Println(elements[i])
+	}
+	*/
+}
