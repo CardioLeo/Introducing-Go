51369bac9e499f35777ab0091a7ca9e33fea51b6
echocslsk <CardioLeo@gmail.com>
2025-01-15 14:50:33 -0700

    brings a seventh bit of code regarding maps to the point of working, from pages 38-9

---
+++ ch05/ch05-07.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//	var x map[string]int
+	//	x["key"] = 10
+	//	fmt.Println(x)
+	x := make(map[string]int)
+	x["key"] = 10
+	fmt.Println(x["key"])
+
+	y := make(map[int]int)
+	y[1] = 10
+	fmt.Println(y[1])
+}
+
