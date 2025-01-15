3a5784b4ebd6a09980bcc7f02837fc3d5209281a
echocslsk <CardioLeo@gmail.com>
2025-01-15 14:39:44 -0700

    adds a lot more code from chapter 5 to ch05-04 doc

--- ch05/ch05-04.go
+++ ch05/ch05-04.go
@@ -24,4 +24,24 @@ func main() {
 	// var x []float64 // this is a slice!
 			// because of the missing array length!
 			// really good to know!
+	x := make([]float64, 5)
+	y := make([]float64, 5, 10)
+	arr := [5]float64{1, 2, 3, 4, 5}
+	z := arr[0:5]
+	// a := [3]float64{x, y, z,}
+	var second_total float64
+	for _, value := range x {
+		second_total += value
+	}
+	fmt.Println(second_total / float64(len(x)))
+	var third_total float64
+	for _, value := range y {
+		third_total += value
+	}
+	fmt.Println(third_total / float64(len(y)))
+	var fourth_total float64
+	for _, value := range z {
+		fourth_total += value
+	}
+	fmt.Println(fourth_total / float64(len(z)))
 }
