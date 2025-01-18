759b9083b34ba28925e63e1f976df6983cbc0076
echocslsk <CardioLeo@gmail.com>
2025-01-18 10:03:20 -0700

    updates the code, tries a couple different things; reshapes how it receives input from scanf; adds a few comments about my dissatisfaction with it.

--- ch06/ch06-07.go
+++ ch06/ch06-07.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-func factorial(x int) int {
+func factorial(x int64) int64 {
 	if x == 0 {
 		return 1
 	}
@@ -12,14 +12,19 @@ func factorial(x int) int {
 	return x * factorial(x-1)
 }
 
-func ask_for_input() int {
+func ask_for_input() int64 {
 	fmt.Println("What number would you like to search for the factorial for?")
-	input, ok := fmt.Scanln()
-	if ok == nil {
-		panic("ok is equal to nil")
-	}
+	var input int64
+	fmt.Scanf("%f", &input)
 	return input
 }
+
+// this doesn't really run like I want it to
+// 1. it won't take more than a single digit as input
+// 2. Won't ask for input at all if I use Scanln (scanf > scanln)
+// 3. it doesn't seem to be the full logic for a factorial?
+// (I just based what I wrote on what the book provided)
+// 
 
 func main() {
 	input := ask_for_input()
