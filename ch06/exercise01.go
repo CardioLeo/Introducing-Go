d702969f976c581d5648355b3a06d17118d4dd3c
echocslsk <CardioLeo@gmail.com>
2025-01-25 11:02:30 -0700

    creates loop and slice and so on for exercise

--- ch06/exercise01.go
+++ ch06/exercise01.go
@@ -4,8 +4,17 @@ import (
 	"fmt"
 )
 
-func sum([]inputs int) int {
-	for _ in range inputs {
-		// too sleepy right now	
+func sum(inputs []int) int {
+	var newSum int = 0
+	for i := 0; i < len(inputs); i++ {
+		fmt.Println(inputs[i])
+		newSum += inputs[i]
 	}
+	return newSum
+}
+
+func main() {
+	mySlice := make([]int, 3, 3)
+	mySum := sum(mySlice)
+	fmt.Println(mySum)
 }
