5c08d08adfb0f1ce507054876f19a467ba282c27
CardioLeo <CardioLeo@gmail.com>
2025-01-25 17:37:10 -0700

    works out a recursive version of a fibonacci function; which honestly is such a mess after you print out the result of every call. Like, wow.

--- ch06/exercise05.go
+++ ch06/exercise05.go
@@ -14,7 +14,28 @@ func fib(times int) {
 	}
 }
 
+func recursive_fib(times int) int {
+	switch times {
+		case 0:
+			return 0
+		case 1:
+			return 1
+		default:
+			return recursive_fib(times - 1) + recursive_fib(times - 2)
+			// temp := recursive_fib(times - 1) + recursive_fib(times - 2)
+			// fmt.Println(temp)
+			// return temp
+
+			// these commented out lines show just how efficient my other
+			// solution to this problem was - even though I solved it that
+			// way initailly due to anxiety from the way the recursive 
+			// one was working
+	}
+}
+
 func main() {
-	// fib(5)
+	fib(5)
 	fib(13)
+
+	fmt.Println(recursive_fib(13))
 }
