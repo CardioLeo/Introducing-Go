3a1d339a6433694746d070ffeb4159ca097ca439
CardioLeo <CardioLeo@gmail.com>
2025-01-28 00:42:26 -0700

    adds a to-do go program just to remind me of a few things to do so I don't, well, forget and move on too soon

---
+++ to-do.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("-- 01 -- finish exercise 04, in ch08")
+	fmt.Println("-- 02 -- look for code from MostNotable from chapters 7 and 8 to add to the MostNotable folder")
+	fmt.Println("-- 03 -- finish the chapter on testing, so I can move onto Kernighan and Pike")
+	fmt.Println("-- 04 -- consider figuring out how to make more code from chapter 8 work - specifically the web and cli stuff")
+}
