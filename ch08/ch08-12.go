a40163ab9416c02f81b9ee5bf195cd372063b706
CardioLeo <CardioLeo@gmail.com>
2025-01-27 23:44:13 -0700

    adds print statements both to main and to the error checking to make it even more clear exactly where the error is occuring; will have to revisit it another time

--- ch08/ch08-12.go
+++ ch08/ch08-12.go
@@ -10,6 +10,7 @@ func server() {
 	// listen on a port
 	ln, err := net.Listen("tcp", ":8080")
 	if err != nil {
+		fmt.Println("error with listening at server()")
 		fmt.Println(err)
 		return
 	}
@@ -17,6 +18,7 @@ func server() {
 		// accept a connection
 		c, err := ln.Accept()
 		if err != nil {
+			fmt.Println("error with accepting connection at server()")
 			fmt.Println(err)
 			continue
 		}
@@ -30,6 +32,7 @@ func handleServerConnection(c net.Conn) {
 	var msg string
 	err := gob.NewDecoder(c).Decode(&msg)
 	if err != nil {
+		fmt.Println("error at decoding message within handleServerConnection()")
 		fmt.Println(err)
 	} else {
 		fmt.Println("Received", msg)
@@ -41,6 +44,7 @@ func client() {
 	// connect to the server
 	c, err := net.Dial("tcp", "127.0.0.1:8080") // book uses port ":9999";
 	if err != nil {
+		fmt.Println("error at calling net.Dial within client()")
 		fmt.Println(err)
 		return
 	}
@@ -50,6 +54,7 @@ func client() {
 	fmt.Println("Sending", msg)
 	err = gob.NewEncoder(c).Encode(msg)
 	if err != nil {
+		fmt.Println("error at sending message from client()")
 		fmt.Println(err)
 	}
 
@@ -57,15 +62,27 @@ func client() {
 }
 
 func main() {
+	var input string
+
 	go server()
 	fmt.Println("Server started")
 	go client()
 	fmt.Println("Client started")
 
 	fmt.Println("starting up!")
-	var input string
+	
+	// var input string
 	fmt.Scanln(&input)
 }
 
 // this consistently gets the error message:
 // "dial tcp 127.0.0.1:8080: connect: connection refused"
+
+// if the code isn't running, port :8080 says: "This site can't be reached"
+// but when the code is running, Chrome tells me the following:
+// "This page isn’t working
+// localhost didn’t send any data.
+// ERR_EMPTY_RESPONSE"
+
+// I added print statements throughout to make it more clear exactly where the error
+// was occuring, and got this back: "error at calling net.Dial within client()"
