7ecaa886fc056d047c3d98989ea08a88629191c5
CardioLeo <CardioLeo@gmail.com>
2025-01-27 22:38:13 -0700

    includes the linked list from page 71; prints it out

--- ch08/ch08-07.go
+++ ch08/ch08-07.go
@@ -3,9 +3,20 @@ package main
 import (
 	"errors"
 	"fmt"
+	"container/list"
 )
 
 func main() {
-	err := errors.New("Error message")
-	fmt.Println(err)
+	err_1 := errors.New("Error message")
+	fmt.Println(err_1)
+
+	var x list.List
+	x.PushBack(1)
+	x.PushBack(2)
+	x.PushBack(3)
+
+	for e := x.Front(); e != nil; e=e.Next() {
+		fmt.Println(e.Value.(int))
+	}
+
 }
