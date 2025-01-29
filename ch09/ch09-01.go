9836c3d4bb632d9023b99a154fe4d00de597be9b
CardioLeo <CardioLeo@gmail.com>
2025-01-28 21:42:02 -0700

    adds else clause for no errors found

--- ch09/ch09-01.go
+++ ch09/ch09-01.go
@@ -9,6 +9,8 @@ func TestAverage(/* t *testing.T */) {
 	v := Average([]float64{1, 2})
 	if v != 1.5 {
 		fmt.Println("Error: Expected 1.5, got ", v)
+	} else {
+		fmt.Println("No errors found")
 	}
 }
 
