24b6fa72eb9844bccdb3bb30cf192d108e26cfdf
CardioLeo <CardioLeo@gmail.com>
2025-01-28 21:41:16 -0700

    first commit for chapter nine, on testing; brings some of the code over from exercise4 of chapter 8; changes some of the code from what the book says - although I probably should change it back. Actually, yes. I should

---
+++ ch09/ch09-01.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+//	"testing"
+)
+
+func TestAverage(/* t *testing.T */) {
+	v := Average([]float64{1, 2})
+	if v != 1.5 {
+		fmt.Println("Error: Expected 1.5, got ", v)
+	}
+}
+
+func Average(xs []float64) float64 {
+	total := 0.0
+	for _, v := range xs {
+		total += v
+	}
+	return total / float64(len(xs))
+}
+
+func maximum(xs []float64) float64 {
+	temp := 0.0
+	for _, v := range xs {
+		if v > temp {
+			temp = v
+			fmt.Println("the new maximum is", temp)
+		}
+	}
+	return temp
+}
+
+func minimum(xs []float64) float64 {
+	temp := 100.0
+	for _, v := range xs {
+		if v < temp {
+			temp = v
+			fmt.Println("the new minimum is", temp)
+		}
+	}
+	return temp
+}
+
+func main() {
+	xs := []float64{64, 83, 77, 82, 84, 54, 98}
+	fmt.Println(Average(xs))
+	
+	maximum(xs)
+	minimum(xs)
+	TestAverage()
+}
