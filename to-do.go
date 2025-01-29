61f71e98464ce5f75681cb942027d93d1b9d692d
CardioLeo <CardioLeo@gmail.com>
2025-01-28 21:49:31 -0700

    updates item one on to-do list

--- to-do.go
+++ to-do.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	fmt.Println("-- 01 -- finish exercise 04, in ch08")
+	fmt.Println("XX 01 XX finish exercise 04, in ch08 ---- done")
 	fmt.Println("-- 02 -- look for code from MostNotable from chapters 7 and 8 to add to the MostNotable folder")
 	fmt.Println("-- 03 -- finish the chapter on testing, so I can move onto Kernighan and Pike")
 	fmt.Println("-- 04 -- consider figuring out how to make more code from chapter 8 work - specifically the web and cli stuff")
