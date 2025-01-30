b15ccce8d69e53fdd9632192f1f6bc210f78054a
CardioLeo <CardioLeo@gmail.com>
2025-01-30 00:57:52 -0700

    makes item three of the list more accurate - that I needed to finish up typing up all of the code in the book, which I have now essentially finished

--- to-do.go
+++ to-do.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	fmt.Println("XX 01 XX finish exercise 04, in ch08 ---- done")
 	fmt.Println("-- 02 -- look for code from MostNotable from chapters 7, 8, 9, & 10 to add to the MostNotable folder")
-	fmt.Println("XX 03 XX finish the chapter on testing, so I can move onto Kernighan and Pike ---- done")
+	fmt.Println("XX 03 XX finish typing up and running all code in the book, so I can move onto Kernighan and Pike ---- done")
 	fmt.Println("-- 04 -- consider figuring out how to make more code from chapter 8 work - specifically the web and cli stuff")
 	fmt.Println("-- 05 -- look over the exercises for chapter 9 & 10 - maybe do one or two")
 }
