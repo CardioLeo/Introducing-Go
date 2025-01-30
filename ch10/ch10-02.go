545724a9041d10d9dcbb968d5f86d04f27ac790e
CardioLeo <CardioLeo@gmail.com>
2025-01-30 00:01:31 -0700

    adds comment based on what the book says about durations and goroutines

--- ch10/ch10-02.go
+++ ch10/ch10-02.go
@@ -12,7 +12,10 @@ func f(n int) {
 	for i := 0; i < 10; i++ {
 		fmt.Println(n, ":", i, ":", n2)
 		n2++
-		amt := time.Duration(rand.Intn(250))
+		amt := time.Duration(rand.Intn(250)) // using the duration makes the goroutines
+							// run simultaneously, whereas
+							// the earlier program only ran one
+							// goroutine at at time
 		time.Sleep(time.Millisecond * amt)
 	}
 }
